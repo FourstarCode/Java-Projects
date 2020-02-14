8aab6845a75e2752d3872d0a76a43b722f5bf46e
Client 0120 <Client 0120@DESKTOP-59ELKUT>
2020-02-14 10:45:27 +0000

    database practice

---
+++ src/database/GetData.java
@@ -0,0 +1,51 @@
+package database;
+
+import static java.lang.System.out;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.text.NumberFormat;
+
+public class GetData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NumberFormat currency = NumberFormat.getCurrencyInstance();
+		
+		final String DRIVER = 
+				"org.apache.derby.jdbc.EmbeddedDriver";
+		final String CONNECTION = 
+				"jdbc:derby:AccountDatabase";
+		
+		try {
+			Class.forName(DRIVER).newInstance();
+		}catch(InstantiationException e) {
+			e.printStackTrace();
+		}catch(IllegalAccessException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try (Connection connection = DriverManager.getConnection(CONNECTION);
+				Statement statement = connection.createStatement();
+				ResultSet resultset = statement.executeQuery("select * from ACCOUNTS")){
+			
+			while(resultset.next()) {
+				out.print(resultset.getString("NAME"));
+				out.print(", ");
+				out.print(resultset.getString("ADDRESS"));
+				out.print(" ");
+				out.println(currency.format(resultset.getFloat("BALANCE")));
+				
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+				
+				
+	}
+
+}
